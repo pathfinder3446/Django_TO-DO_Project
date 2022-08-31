c7fa063f17fe498589a7a205bf4476a51f7b381b
Arif <kanebrian0634@gmail.com>
2022-08-31 20:40:20 +0300

    ...

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from todo.models import Todo
 from .forms import TodoForm
+from django.contrib import messages
 
 def home(request):
     todos = Todo.objects.all()
@@ -19,6 +20,7 @@ def todo_create(request):
         form = TodoForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request,"Todo created successfully")
             return redirect("home")
 
     context = {
@@ -30,8 +32,26 @@ def todo_update(request,id):
     todo = Todo.objects.get(id=id)
     form = TodoForm(instance=todo)
 
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
     context = {
         "todo" : todo,
         "form" : form
     }
     return render(request, "todo/todo_update.html", context)
+
+def todo_delete(request,id):
+    todo = Todo.objects.get(id=id)
+    if request.method == "POST":
+        todo.delete()
+        messages.warning(request, "Todo deleted!")
+        return redirect("home")
+
+    context = {
+        "todo" : todo,
+    }
+
+    return render(request, "todo/todo_delete.html", context)
